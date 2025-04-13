30ce8dc183803bb9df1090acbc528e09caea350d
Mydaudat <binhok106@gmail.com>
2025-04-13 15:23:13 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyTools',
+    version='1.0.1',
+    packages=find_packages(),
+    description='Provides some useful functions.',
+    author='Mydaudat',
+    author_email='binhok106@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
